e04f8050e479801299a1946779d513f13b2c6a92
Carmen Wick <CarmenWick@gmail.com>
2015-02-26 11:00:41 -0800

    First Commit

---
+++ MOSSim/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  MOSSim
+//
+//  Created by Carmen Wick on 2/26/15.
+//  Copyright (c) 2015 Carmen Wick. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
